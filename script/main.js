147bdbd4659a24e9c1c217d79afc7a7d2e84dca0
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-05-04 20:10:19 +0800

    - changed css on menu - add cart popup (still ongoing) - fixed menu grid

--- script/main.js
+++ script/main.js
@@ -20,11 +20,10 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-
-})
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+});
