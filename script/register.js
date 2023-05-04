a713645f3ba2f1c5121ae0c41812da8f3e24e90c
lutfilhadi <lutfilhadiabdulrahim@gmail.com>
2023-05-04 13:00:17 +0800

    register input validity js

--- script/register.js
+++ script/register.js
@@ -1,7 +1,47 @@
+const firstNameInput = document.getElementById('fname');
+const lastNameInput = document.getElementById('lname');
+const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
+const registerButton = document.getElementById('register-btn');
+
+const nameInputAlert = document.getElementById('name-input-alert');
+const usernameInputAlert = document.getElementById('username-input-alert');
 const confirmPasswordInput = document.getElementById('confirmPassword');
 const passwordRequirementAlert = document.getElementById('password-requirement-alert');
 const passwordMatchAlert = document.getElementById('password-match-alert');
+
+const nameRegex = /^[A-Za-z]+$/; // regex for alphabetical characters only
+
+let isFirstNameValid = false;
+let isLastNameValid = false;
+let isUsernameValid = false;
+let isPasswordValid = false;
+
+firstNameInput.addEventListener('input', () => {
+  if (!nameRegex.test(firstNameInput.value)) {
+    nameInputAlert.style.display = 'inline';
+  } else {
+    nameInputAlert.style.display = 'none';
+  }
+  enableRegisterButton();
+});
+
+lastNameInput.addEventListener('input', () => {
+  if (!nameRegex.test(lastNameInput.value)) {
+    nameInputAlert.style.display = 'inline';
+  } else {
+    nameInputAlert.style.display = 'none';
+  }
+});
+
+usernameInput.addEventListener('input', () => {
+  if (usernameInput.value.length < 4) {
+    usernameInputAlert.style.display = 'inline';
+  } else {
+    usernameInputAlert.style.display = 'none';
+  }
+  enableRegisterButton();
+});
 
 confirmPasswordInput.addEventListener('input', function() {
   if (confirmPasswordInput.value !== passwordInput.value) {
@@ -9,6 +49,7 @@ confirmPasswordInput.addEventListener('input', function() {
   } else {
     passwordMatchAlert.style.display = 'none';
   }
+  enableRegisterButton();
 });
 
 passwordInput.addEventListener('input', () => {
@@ -19,4 +60,13 @@ passwordInput.addEventListener('input', () => {
     } else {
         passwordRequirementAlert.style.display = 'none';
     }
+    enableRegisterButton();
   });
+
+  function enableRegisterButton() {
+    if (isFirstNameValid && isLastNameValid && isUsernameValid && isPasswordValid) {
+      registerButton.disabled = false;
+    } else {
+      registerButton.disabled = true;
+    }
+  }
