f04614a143cc1ad3e7ef2dfb0eafb9563449e613
ssebin <96987066+ssebin@users.noreply.github.com>
2023-05-04 16:12:31 +0800

    homepage sliding pics

--- script/main.js
+++ script/main.js
@@ -1,18 +1,30 @@
-// initialize swiper js
+// homepage sliding pics
+
+var counter = 2;
+document.getElementById('radio1').checked = true;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
+
+// review initialize swiper js
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
+  loop: true,
 
-     // If we need pagination
+  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
 
 })
