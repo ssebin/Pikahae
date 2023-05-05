fcc658a166a24d1efba5ee1f37fd01d805f5e109
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-05-05 21:50:35 +0800

    - add scroll to top btn

--- script/menu.js
+++ script/menu.js
@@ -312,6 +312,28 @@ modalBtns.forEach((modalBtn) => {
     });
 });
 
+// scroll to top
 
+// Get the button:
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to 100px from the top of the document
+function topFunction() {
+    window.scrollTo({
+        top: 300,
+        behavior: 'smooth'
+    });
+}
 
 
